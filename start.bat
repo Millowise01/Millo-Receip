5e2cf7057ee08c707f8cff4700196efc817b98a9
Leroy <l.carew@alustudent.com>
2025-08-04 23:09:19 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,13 @@
 @echo off
+echo Setting environment variables for development...
+set API_KEY=secure-api-key-2024
+set PORT=8080
+set NODE_ENV=development
+
+echo.
 echo Installing dependencies...
 npm install
 
+echo.
 echo Starting the server...
 npm start
